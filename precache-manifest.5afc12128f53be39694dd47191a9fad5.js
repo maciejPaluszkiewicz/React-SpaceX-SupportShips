f1fcf5110d084a9f22d5669473e85e1101a0001c
maciejPaluszkiewicz <maciej.j.paluszkiewicz@gmail.com>
2019-07-13 22:49:15 +0200

    Updates

---
+++ precache-manifest.5afc12128f53be39694dd47191a9fad5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "699b7f5a0ae208444c7baebb2064e4b4",
+    "url": "/React-SpaceX-SupportShips/static/media/spaceXlogo.699b7f5a.png"
+  },
+  {
+    "revision": "f1075a3070b764d034d763676acd8d37",
+    "url": "/React-SpaceX-SupportShips/static/media/deepspace.f1075a30.jpg"
+  },
+  {
+    "revision": "5ece6999d7e311da8fb8",
+    "url": "/React-SpaceX-SupportShips/static/js/runtime~main.6e120e81.js"
+  },
+  {
+    "revision": "439228675060950d55d1",
+    "url": "/React-SpaceX-SupportShips/static/js/main.06ed74e0.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/React-SpaceX-SupportShips/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "439228675060950d55d1",
+    "url": "/React-SpaceX-SupportShips/static/css/main.dc92d054.chunk.css"
+  },
+  {
+    "revision": "7dd8ac7ff40e192ba08169c9421dc619",
+    "url": "/React-SpaceX-SupportShips/index.html"
+  }
+];
